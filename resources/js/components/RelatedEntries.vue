02b1767978ee6fd491b09fbf94a03800ec1aad8c
Sairahcaz <zacharias.creutznacher@gmail.com>
2023-01-29 12:22:06 +0100

    #7 - sorting icons 2.0

--- resources/js/components/RelatedEntries.vue
+++ resources/js/components/RelatedEntries.vue
@@ -107,8 +107,8 @@
                                 </span>
                             </span>
                             <span v-else>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+                                <svg style="color:gray;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                                    <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
                                 </svg>
                             </span>
                         </span>
